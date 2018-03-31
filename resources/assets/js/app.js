da6ed50028b8f88c10e854b2514c61e1ecbf3408
diegorodsouza <diegorodsouza@hotmail.com>
2018-03-31 12:15:12 -0300

    +modais

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,6 +21,9 @@ Vue.component('painel', require('./components/Painel.vue'));
 Vue.component('caixa', require('./components/Caixa.vue'));
 Vue.component('tabela-lista', require('./components/TabelaLista.vue'));
 Vue.component('migalhas', require('./components/Migalhas.vue'));
+Vue.component('modal', require('./components/modal/Modal.vue'));
+Vue.component('modal-link', require('./components/modal/ModalLink.vue'));
+Vue.component('formulario', require('./components/Formulario.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,8 +4,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+
               <painel titulo="Artigos">
+
                 <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
+
+                <modal-link tipo="button" nome="olar" titulo="Oie"></modal-link>
+
                 <tabela-lista 
                   v-bind:titulos="['#','Nome','Descrição']" 
                   v-bind:itens="[['1','teste1','titulo1'],['2','teste2','titulo2'],['3','teste3','titulo3']]"
@@ -15,8 +20,33 @@
                   
                   >
                 </tabela-lista>
+
               </painel>
+
             </div>
         </div>
     </div>
+
+    <modal titulo="Oie" nome="olar">
+      <painel titulo="Adicionar">
+        <formulario css="" action="#" method="post" enctype="" token="">
+
+        <form>
+
+            <div class="form-group">
+              <label for="titulo">Título</label>
+              <input type="text" class="form-control" id="titulo" placeholder="Título...">
+            </div>
+
+            <div class="form-group">
+              <label for="descricao">Descrição</label>
+              <input type="text" class="form-control" id="descricao" placeholder="Descrição...">
+            </div>
+
+            <button type="submit" class="btn btn-info">Adicionar</button>
+          </form>
+
+        </formulario>
+      </painel>
+    </modal>
 @endsection
